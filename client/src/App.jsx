0768eb9e9f1724751b22cfd543aed53acc881f52
rioto <rioto@students.cs.ubc.ca>
2025-01-09 20:06:36 -0800

    change react to use vite and add cors in server to allow local testing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,10 @@
-
-
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          hi
-        </p>
-      </header>
-    </div>
-  );
+    <>
+    meow
+    </>
+  )
 }
 
-export default App;
+export default App
