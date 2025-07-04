11f653aa75adf1220305e67016ae78b4a1d239a1
okarioto <151336957+okarioto@users.noreply.github.com>
2025-07-04 16:06:38 -0700

    Update Socials.jsx

--- client/src/components/Socials.jsx
+++ client/src/components/Socials.jsx
@@ -1,12 +1,13 @@
-
+import ig from "../assets/instagram.svg"
+import fb from "../assets/facebook.svg"
 export default function Socials() {
   return (
     <div className="flex w-[60%] justify-between mb-6">
       <a href="https://www.instagram.com/ubc_bc/" target="blank">
-        <img src="../src/assets/instagram.svg" alt="" className="w-7" />
+        <img src={ig} alt="" className="w-7" />
       </a>
       <a href="https://www.facebook.com/ubcbc" target="blank">
-        <img src="../src/assets/facebook.svg" alt="" className="w-7" />
+        <img src={fb} alt="" className="w-7" />
       </a>
     </div>
   );
